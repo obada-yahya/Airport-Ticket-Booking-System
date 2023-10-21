e020485b4acbc810e782ab0a6a67373f7b8ae9a5
Obada-Yahya <obadayahya.an@gmail.com>
2023-10-21 22:32:19 +0300

    added Domain Models : Passenger, Ticket, Flight, FlightManager

---
+++ AirportTicketBookingDomain/Ticket.cs
@@ -0,0 +1,26 @@
+﻿namespace AirportTicketBookingDomain;
+
+public enum TicketStatus
+{
+    Available,
+    Cancelled,
+    Booked,
+}
+
+public enum CabinClass
+{
+    Economy,
+    PremiumEconomy,
+    Business,
+    FirstClass
+}
+
+public class Ticket
+{
+    public Guid TicketId { get; set; }
+    public Guid FlightId { get; set; }
+    public Guid PassengerId { get; set; }
+    public float Price { get; set; } 
+    public TicketStatus Status { get; set; }
+    public CabinClass CabinClass { get; set; }
+}
