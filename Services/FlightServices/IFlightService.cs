ed7587aa355330f70dc8ec045912b27c522f8401
Obada-Yahya <obadayahya.an@gmail.com>
2023-11-18 12:24:56 +0200

    added Flight services

---
+++ Services/FlightServices/IFlightService.cs
@@ -0,0 +1,14 @@
+﻿using AirportTicketBookingDomain;
+
+namespace AirportTicketBooking.Services.FlightServices;
+
+public interface IFlightService
+{
+    public void AddFlight(Flight flight);
+    public Flight? FindFlight(string id);
+    public IEnumerable<Flight> GetFlights();
+    public void UpdateFlight(Flight flight);
+    public void DeleteFlight(string id);
+    public IEnumerable<Flight> GetManagedFlightByManager(string flightManagerId);
+    public void UnassignManagerFromFlights(string flightManagerId);
+}
